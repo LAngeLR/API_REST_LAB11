1cf39cf3f09c4b8e59f7aee37562da1f09c7901d
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-11-04 06:17:05 -0500

    Webconfig añadido

--- Lab11_GTICS_Base_2023-2/src/main/java/edu/pucp/gtics/lab11_gtics_20232/controller/DistribuidorasController.java
+++ Lab11_GTICS_Base_2023-2/src/main/java/edu/pucp/gtics/lab11_gtics_20232/controller/DistribuidorasController.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/distribuidora")
 
 public class DistribuidorasController {
